e24aa0d7ca5933ffe476ad8a9c0791c815169a7c
Max Zhivaev <dizas@me.com>
2020-01-21 21:53:15 +0300

    added loggers

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+const { NODE_ENV, DB_ADDRESS } = process.env;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { celebrate, errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { createUserValidation, loginValidation } = require('./variables/validation');
 const auth = require('./middlewares/auth');
 const routes = require('./routes');
@@ -13,7 +16,7 @@ const { createUser, login } = require('./controllers/user');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/news-explorer-db', {
+mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://localhost:27017/news-explorer-db', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -24,14 +27,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.post('/signup', celebrate(createUserValidation), createUser);
 app.post('/signin', celebrate(loginValidation), login);
 
 app.use(auth);
 app.use('/', routes);
 
-app.use(errors());
+app.use(errorLogger);
 
+app.use(errors());
 app.use(handleError);
 
 app.listen(PORT, () => {
