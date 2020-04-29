211d68c2ba03185e7944791f490a08827261b1cc
Max Zhivaev <dizas@me.com>
2020-04-29 09:12:16 +0300

    fixed url gh-pages

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'https://news-explorer.xyz', 'https://dissidance.github.io/news-explorer-frontend'],
+  origin: ['http://localhost:8080', 'https://news-explorer.xyz', 'https://dissidance.github.io'],
   credentials: true,
 };
 
